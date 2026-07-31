65024d7d9098708f37f09f90edfacce51b769a6a
abdul razzaq ansari <109792351+MRRAZK@users.noreply.github.com>
2026-07-31 21:27:13 +0530

    Add files via upload

---
+++ data_type.dart
@@ -0,0 +1,36 @@
+// Numbers, String, booleans, lists, maps, dynamic
+
+
+
+
+void main(){
+
+  // Strings/
+  String First_name = "Anasri";
+  print("My First name is : $First_name");
+
+  // int
+  int integer = 41;
+  print("The integer value is : $integer");
+
+  //double
+  double d = 33.33;
+  print("The double value is : $d");
+
+  // num 
+  num num1 = 44.44;
+  num num2 = 44;
+  print("First num is : $num1");
+  print("Second num is : $num2");
+
+  // Boolean
+  bool Mybool = true;
+  print("The Boolean value is : $Mybool");
+
+  // Dynamic
+  dynamic fullname = "razzaq";
+  dynamic dynamic_integer = 41;
+  dynamic dynamic_boolans = true;
+  print("$dynamic_boolans $dynamic_integer $fullname");
+
+}
---
+++ first.dart
@@ -0,0 +1,23 @@
+void main(){
+  // varibales 
+  var name = "razzaq";
+
+  // integer
+  int num1 = 20;
+
+  // String
+  String first_name = "Ansari";
+
+  // const
+  const constant = "ansari";
+  final f = "ansari";
+  print(f);
+  print(constant);
+  // decalaring and not using
+  var Yourname;
+
+  print(Yourname);
+
+  print("$name $num1 $first_name");
+
+}
