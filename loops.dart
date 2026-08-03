4b64f008605aabaefb996702a4410ecd01c197eb
abdul razzaq ansari <109792351+MRRAZK@users.noreply.github.com>
2026-08-03 19:56:45 +0530

    Add files via upload

---
+++ loops.dart
@@ -0,0 +1,21 @@
+//loops
+void main(){
+  //for loop
+  for (var i = 1; i <= 5;i++){
+    print(i);
+  }
+
+  //for in loop
+  var names = ["razzaq", "hannan", "nawaaz"];
+  for (var name in names){
+    print(name);
+  }
+
+  //while 
+  var count = 1;
+  while(count <=5){
+    print(count);
+    count++;
+  }
+  
+}
