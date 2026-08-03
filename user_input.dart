683e6f4e570c60647476e55b1556f1f68d48f7a0
abdul razzaq ansari <109792351+MRRAZK@users.noreply.github.com>
2026-08-03 20:40:58 +0530

    Add files via upload

---
+++ user_input.dart
@@ -0,0 +1,14 @@
+//user input
+
+import 'dart:io';
+
+void main(){
+
+  print("Enter your name : ");
+  var name = stdin.readLineSync();
+
+  print("Enter your more name : ");
+  String? name1 = stdin.readLineSync();
+  print(name);
+  print(name1);
+}
