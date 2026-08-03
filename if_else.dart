cdaabcb9c94a9bd6ef90ee634e0624cafa81005a
abdul razzaq ansari <109792351+MRRAZK@users.noreply.github.com>
2026-08-03 20:07:54 +0530

    Add files via upload

---
+++ if_else.dart
@@ -0,0 +1,20 @@
+
+void main(){
+  int num = 7;
+
+  if (num == 5){
+    print("the number is 5");
+  } else {
+    print("the number is not 5");
+  }
+// if else ladder
+  if (num == 5){
+    print("the number is 5");
+  } 
+  else if(num == 4) {
+    print("the number is $num");
+  }
+  else {
+    print("The end not found");
+  }
+}
